09677c551770bd75ae6ae1f185bf6258e76fbf2e
Jocelyn <jocelyn725paek@gmail.com>
2023-06-14 12:46:46 -0700

    Home Page for Mind The Gap project

--- Mind the Gap/ContentView.swift
+++ Mind the Gap/ContentView.swift
@@ -12,34 +12,55 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
            ZStack {
-                Color(.blue)
+                Color("neutral")
                     .ignoresSafeArea()
-                VStack {
+             
+                  
+               
+                VStack(alignment: .center, spacing: 20.0) {
                     Image("logo")
-                    Text("Mind the Gap")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                  //  Spacer()
                     
                     NavigationLink(destination: AddressIssueView()) {
                         Text("Address the Issue")
+                            .fontWeight(.bold)
                             .foregroundColor(Color.black)
                             .multilineTextAlignment(.center)
+                         //   .overlay(
+                           //     Capsule(style: .continuous)
+                             //       .stroke(Color."LPink", lineWidth: 5)
+                            //)
+                        //    .buttonStyle(.borderedProminent)
+                            .frame(width: 170, height: 60, alignment: .center)
+                            .background()
+                            .cornerRadius(10)
+                          //  .padding()
                         
                     }
                     .padding()
                     
                     NavigationLink(destination: ComparisonsView()) {
                         Text("Comparisons")
+                            .fontWeight(.bold)
                             .foregroundColor(Color.black)
-                        
                             .multilineTextAlignment(.center)
+                            .frame(width: 170, height: 60, alignment: .center)
+                            .background()
+                            .cornerRadius(10)
+                        
                         
                     }
                     .padding()
                     NavigationLink(destination: AwarenessHelpView()) {
                         Text("Awareness and Ways to Help")
+                            .fontWeight(.bold)
                             .foregroundColor(Color.black)
                             .multilineTextAlignment(.center)
+                            .frame(width: 170, height: 60, alignment: .center)
+                            .background()
+                            .cornerRadius(10)
                     }
                     .padding()
                 }
