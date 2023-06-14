981495df540fa0ad82dcb5728fb1a89ad33c33eb
Jocelyn <jocelyn725paek@gmail.com>
2023-06-14 11:53:27 -0700

    Initial Commit

---
+++ Mind the Gap/Mind_the_GapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mind_the_GapApp.swift
+//  Mind the Gap
+//
+//  Created by Jocelyn Paek on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Mind_the_GapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
